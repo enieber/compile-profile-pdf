65e0936bb227294d175dc14571ef447b33a67011
Enieber Cunha <7907068+enieber@users.noreply.github.com>
2023-03-30 16:22:50 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const markdownpdf = require("markdown-pdf")
 const fs = require("fs")
  
+/*
 markdownpdf()
   .from("profile.md")
   .to("profile.pdf", function () {
     console.log("Done")
   })
-
+*/
 
 markdownpdf()
   .from("profile-pt.md")
